ab69a8a0bc6c85498762d18f36a2671273f7ebc2
nshuti jonathan <nshutijonathan130@gmail.com>
2019-02-06 17:19:15 -0800

    Delete server.ts

--- server.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-const app=express();
-
-app.get("/",(req, res, next) =>{
-    res.send("tour booking API");
-});
-
-app.listen(process.env.PORT || 8091,()=>console.log("server started..."));
